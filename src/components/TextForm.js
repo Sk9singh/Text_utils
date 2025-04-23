2d8cb0457555b273fa0ad27a43fc2669e33a09b7
Saksham Singh <sakshamsingh8278@gmail.com>
2025-04-24 02:51:47 +0530

    Javascript logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,19 +7,34 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleDownClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
 
-   const [text,setText] = useState('Enter text here')
+   const [text,setText] = useState('')
 
   return (
-    <div>
+   <>
+    <div className='container'>
         <h1>{props.heading}</h1>
          <div className="mb-3">
          <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button> 
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button> 
+        <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button> 
     </div>
+      <div className="container my-3">
+       <h2>Your Text Summary</h2>
+         <p>{text.split(" ").length} words and {text.length} characters</p>
+         <p>{0.008 * text.split(" ").length} avg Minutes to read</p>
+         <h2>Preview</h2>
+         <p>{text}</p>
+      </div>
+   </>
   )
 }
