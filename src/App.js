c8e2b1219f86f2284e9c0d4125e33061347333a2
Saksham Singh <sakshamsingh8278@gmail.com>
2025-04-24 02:18:17 +0530

    logic added for text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
 import './App.css';
 import Navbar from './components/Navbar.js';
+import TextForm from './components/TextForm.js';
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils" about="About"/>
+    <div className="container my-3">
+      <TextForm heading="Enter the text to analyze"/>   
+    </div>
     </>
   );
 }
